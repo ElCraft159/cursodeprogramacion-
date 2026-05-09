8793824cad2dc5fe6d18a90f5ad260a1c15f4960
ElCraft159 <wullianpeley@gmail.com>
2026-05-09 17:29:22 -0400

    conteo

--- Tarea viernes 15/1 Conteo basico.py
+++ Tarea viernes 15/1 Conteo basico.py
@@ -3,6 +3,7 @@ for x in range(1, 16):
     print(x)
 
 # Conteo regresivo
-while n := int(input("Ingrese un número para contar hacia atrás: ")) > 0:
-    for x in range(n, 0, -1):
-        print(x)
+num = int(input("Ingrese un número para contar hacia atrás: "))
+while num >= 0:
+    print(num)
+    num -= 1
