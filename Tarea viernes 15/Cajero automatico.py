11e2e649dd1780f1641e11c98d063ca69f09be62
ElCraft159 <wullianpeley@gmail.com>
2026-05-12 11:44:53 -0400

    Mas retoques

--- Tarea viernes 15/Cajero automatico.py
+++ Tarea viernes 15/Cajero automatico.py
@@ -4,18 +4,22 @@ print("_" * 50 + "")
 saldo = 1000
 pin = ""
 print("Create una cuenta")
-usuario = input("Ingresar tu usuario: ").upper()
+usuario = input("Ingresar tu nombre: ").upper()
 intentos = 3
+print("_" * 50 + "")
 print(f"Crea una contraseña de 4 digitos numericos tienes {intentos} intentos")
 while intentos > 0:
     pin = int(input("Ingrese un pin: "))
     if pin >= 1000 and pin <= 9999:
+         print("_" * 50 + "")
          print("Contrasena creada exitosamente")
          break
     else:
+        print("_" * 50 + "")
         print("Pin incorrecto")
         intentos -= 1
     if intentos == 0:
+        print("_" * 50 + "")
         print("Has agotado tus intentos")
         exit()            
 print("_" * 50 + "")
@@ -26,13 +30,17 @@ intento = 3
 while intento > 0:
     contra = int(input("Ingrese tu pin: "))
     if contra == pin:
-        print("Sesion iniciada")
+        print("_" * 50 + "")
+        print("\n""Sesion iniciada")
         break
     else:
+        print("_" * 50 + "")
         print("Contrasena incorrecta")
         intento -= 1
     if intento == 0:
+        print("_" * 50 + "")
         print("Usuario bloqueado")
+        print("_" * 50 + "")
         exit()
 print("_" * 50 + "")
 print("Usuario", usuario)
@@ -48,13 +56,18 @@ while True:
     print("_" * 50 + "")
     opcion = int(input("Ingrese una opcion: "))
     if opcion == 1:
+        print("_" * 50 + "")
         retirar = int(input("Ingrese la cantidad a retirar: "))
         if retirar > saldo:
+            print("_" * 50 + "")
             print("Saldo insuficiente")
+            print("Saldo restante: ", saldo,"$")
         elif retirar % 10 != 0:
+            print("_" * 50 + "")
             print("Solo puedes retirar multiplos de 10") 
         else:
             saldo -= retirar
+            print("_" * 50 + "")
             print(f"Retiro exitoso {retirar}$")
             b100 = retirar // 100
             resto = retirar % 100
@@ -67,16 +80,21 @@ while True:
             print(f"Saldo restante: ", saldo,"$")
         print("_" * 50 + "")
     elif opcion == 2:
+        print("_" * 50 + "")
         depositar = int(input("Ingrese la cantidad a depositar: "))
         saldo += depositar
+        print("_" * 50 + "")
         print("Deposito exitoso")
         print(f"Saldo actual: ", saldo,"$")
         print("_" * 50 + "")
     elif opcion == 3:
+        print("_" * 50 + "")
         print("Tu saldo es: ", saldo,"$")
         print("_" * 50 + "")    
     elif opcion == 4:
+        print("_" * 50 + "")
         print("Gracias por usar el cajero automatico")
+        print("_" * 50 + "")
         break
     else:
         print("Opcion no valida del menu")
